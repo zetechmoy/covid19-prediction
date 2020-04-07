365f6d4c587d656d4af6b884a773e9a07830641f
Théo Guidoux <theoguidoux@gmail.com>
2020-04-07 21:33:39 +0200

    testing evolution

--- test.py
+++ test.py
@@ -1,32 +1,29 @@
 import keras
 import numpy as np
+import matplotlib.pyplot as plt
 
 data_type = "confirmed"
-model_path = "models/20200406-181926/"+data_type+"_model.h5"
+model_path = "models/20200407-123240/"+data_type+"_model.h5"
 
 #china [27.3338, 128.5697],[56.0, 90.0, 114.0] => 146.55475
 #suisse [46.8182, 8.2275],[56.0, 90.0, 114.0] => 146.9167
 
-all_values = [404, 632, 921]
+def draw(xs, ys, color='blue'):
+	for i in range(0, min(len(xs), len(ys))-1):
+		plt.plot([xs[i], xs[i+1]], [ys[i], ys[i+1]], color=color)
+
+#test on simple data
+all_values = [19856,22304,25233]
 
 latlng = np.asarray([46.131357,-2.4364346])
 
-last_evolution = np.asarray(all_values)
+# Recreate the exact same model purely from the file
+model = keras.models.load_model(model_path)
 
-for i in range(0, 10):
-	print(last_evolution)
-	input = [[[last_evolution]], [latlng]]
+input = [[[all_values]], [latlng]]
+prediction = model.predict(input)[0][0]
 
-	# Recreate the exact same model purely from the file
-	model = keras.models.load_model(model_path)
-
-	prediction = model.predict(input)[0][0]
-	next_last_evolution = np.hstack((last_evolution[1:], [prediction]))
-	all_values.append(prediction)
-	last_evolution = next_last_evolution
-
-print(all_values)
-
+print(all_values, "=>", prediction)
 
 #check whether location as a real influence (just a bit)
 #for i in range(0, 180, 1):
@@ -38,5 +35,39 @@ print(all_values)
 #
 #		print([j, i], prediction)
 
+#compare predicted and true evolution
+#feb 15 2020 => 1580515200
+#
+#true_ys = [12,12,12,12,12,12,12,12,12,12,14,18,38,57,100,130,191,204,285,377,653,949,1126,1209,1784,2281,2281,3661,4469,4499,6633,7652,9043,10871,12612,14282,16018,19856,22304,25233,29155,32964,37575,40174,44550,52128,56989,59105,64338,89953]
+#
+#start_x = [12, 12, 12]
+#start_date = 1580515200
+#
+#xs = list()
+#ys = list()
+#latlng = np.asarray([46.131357,-2.4364346])#france
+#last_evolution = np.asarray(start_x)
+#model = keras.models.load_model(model_path)
+#
+##see evolution
+#for i in range(0, len(true_ys)):
+#	input = [[[last_evolution]], [latlng]]
+#
+#	prediction = model.predict(input)[0][0]
+#	next_last_evolution = np.hstack((last_evolution[1:], [prediction]))
+#	last_evolution = next_last_evolution
+#
+#	ys.append(prediction)
+#	xs.append(start_date)
+#
+#	start_date += 86400
+#
+#print(xs)
+#print(ys)
+#
+#draw(xs, ys, color="blue")
+#draw(xs, true_ys, color="green")
+#plt.show()
 
-#[12000, 7788, 2886, 2433.0881, 2132.6492, 1913.0713, 1739.1803, 1610.0355, 1511.5085, 1437.265, 1378.7538, 1332.4576, 1294.2588]
+#on se rend compte que lorsqu'il s'agit de prédire la valeur suivante il n'y a
+#pas de problème mais à long terme les valeurs sont trop fausses (en faisant l'évolution)
